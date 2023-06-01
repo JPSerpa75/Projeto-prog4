d97d87a37babd125a6046fd17241a92640566eaa
guilherme oliveira <guilhermeplay02@hotmail.com>
2023-06-01 16:43:22 -0300

    Consultar cliente feito

--- Persistencia/src/DAO/ClienteDAO.java
+++ Persistencia/src/DAO/ClienteDAO.java
@@ -2,13 +2,16 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import Connection.ConnectionFactoryMySQL;
 import Dominio.Cliente;
+import Dominio.Produto;
 
 public class ClienteDAO {
 
@@ -37,5 +40,102 @@ public class ClienteDAO {
 			JOptionPane.showMessageDialog(j, "Cliente não cadastrado, erro: "+ e.getMessage());
 		}
 	}
+	
+	//Método READ
+		public ArrayList<Cliente> Read(){
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			String sql = "SELECT * FROM cliente";
+			ArrayList<Cliente> clientes = new ArrayList<>();
+			try {
+				stmt = con.prepareStatement(sql);
+				rs = stmt.executeQuery();
+				System.out.println("Dados capturados com sucesso!");
+				while(rs.next()) {
+					Cliente c  = new Cliente();
+					c.setIdCliente(rs.getLong("idCliente"));
+					c.setNome(rs.getString("Nome"));
+					c.setCpf(rs.getString("CPF"));
+					c.setEndereco(rs.getString("Endereço"));
+					c.setTelefone(rs.getString("Telefone"));
+					c.setEmail(rs.getString("email"));
+					clientes.add(c);
+				}
+			}catch(SQLException e) {
+				System.out.println("Falha ao consultar dados, erro: " + e.getMessage());
+			}
+			
+			return clientes;
+			
+		}
+		
+		
+		//Método consultar por descrição
+		public ArrayList<Cliente> ConsultarPorNome(String nome, JFrame j){
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			String sql = "SELECT * FROM cliente WHERE Nome LIKE '%"+ nome +"%'";
+			ArrayList<Cliente> clientes = new ArrayList<>();
+			try {
+				stmt = con.prepareStatement(sql);
+				rs = stmt.executeQuery();
+				System.out.println("Dados capturados com sucesso!");
+				while(rs.next()) {
+					Cliente c  = new Cliente();
+					c.setIdCliente(rs.getLong("idCliente"));
+					c.setNome(rs.getString("Nome"));
+					c.setCpf(rs.getString("CPF"));
+					c.setEndereco(rs.getString("Endereco"));
+					c.setTelefone(rs.getString("Telefone"));
+					c.setEmail(rs.getString("email"));
+					clientes.add(c);
+				}
+			}catch(SQLException e) {
+				JOptionPane.showMessageDialog(j, "Falha ao consultar dados, erro: " + e.getMessage());
+			}
+			
+			return clientes;
+			
+		}
+		
+		//Operação de delete (exclusão)
+		public void delete(String CPF, JFrame j) {
+			PreparedStatement stmt = null;
+			String sql = "DELETE FROM cliente WHERE CPF = ?";
+			try {
+				int confirma = JOptionPane.showConfirmDialog(j, "Confirma a exclusão?", "Selecione uma opção", JOptionPane.YES_NO_OPTION);
+				if(confirma==0) {
+					stmt = con.prepareStatement(sql);
+					stmt.setString(1, CPF);
+					stmt.executeUpdate();
+					JOptionPane.showMessageDialog(j, "Dado excluido com sucesso!");
+				}else {
+					JOptionPane.showMessageDialog(j, "Operação cancelada!");
+				}
+			}catch(SQLException e) {
+				JOptionPane.showMessageDialog(j, "Erro ao excluir: " + e.getMessage() + " codigo lido: " + CPF);
+			}
+		}
+		
+		//Método de update
+		public void update(Cliente c, JFrame j) {
+			PreparedStatement stmt;
+			String sql = "UPDATE cliente SET Nome = ?, email = ? , Endereco = ?, Telefone = ? WHERE idCliente=?";
+			try {
+				stmt = con.prepareStatement(sql);
+				stmt.setString(1, c.getNome());
+				stmt.setString(2, c.getEmail());
+				stmt.setString(3, c.getEndereco());
+				stmt.setString(4, c.getCpf());
+				stmt.setString(5, c.getTelefone());
+				stmt.setFloat(6, c.getIdCliente());
+				
+				stmt.executeUpdate();
+				JOptionPane.showMessageDialog(j, "Dado atualizado com sucesso!");
+			}catch(SQLException e) {
+				JOptionPane.showMessageDialog(j, "Erro ao atualizar dado: " + e.getMessage());
+			}
+			
+		}
 	
 }
---
+++ Persistencia/src/View/TelaConsultaCliente.java
@@ -0,0 +1,167 @@
+package View;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import DAO.ClienteDAO;
+import DAO.ProdutoDAO;
+import Dominio.Cliente;
+import Dominio.Produto;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TelaConsultaCliente {
+
+	private JFrame frame;
+	private JTextField txtNome;
+	private JTable table;
+	
+	
+
+	public JFrame getFrame() {
+		return frame;
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaConsultaCliente window = new TelaConsultaCliente();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	private void atualizaBusca() {
+		ClienteDAO dao = new ClienteDAO();
+		String nome = txtNome.getText();
+		DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
+		tableModel.setNumRows(0);
+		for(Cliente c:dao.ConsultarPorNome(nome, frame)) {
+			tableModel.addRow(new Object[] {
+					c.getIdCliente(),
+					c.getCpf(),
+					c.getNome(),
+					c.getEmail(),
+					c.getTelefone(),
+					c.getEndereco()
+					
+			});
+		}
+		
+		if(tableModel.getRowCount()==0) {
+			JOptionPane.showMessageDialog(frame, "Nenhum produto foi encontrado!");
+			txtNome.setText("");
+		}
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaConsultaCliente() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 403);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 434, 364);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblTitle = new JLabel("Consultar Cliente");
+		lblTitle.setBounds(155, 11, 129, 14);
+		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		panel.add(lblTitle);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setBounds(10, 46, 39, 14);
+		panel.add(lblNome);
+		
+		txtNome = new JTextField();
+		txtNome.setBounds(10, 71, 414, 20);
+		panel.add(txtNome);
+		txtNome.setColumns(10);
+		
+		JButton btnFiltrar = new JButton("Filtrar");
+		btnFiltrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				atualizaBusca();
+			}
+		});
+		btnFiltrar.setBounds(335, 102, 89, 23);
+		panel.add(btnFiltrar);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 148, 414, 131);
+		panel.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Nome", "CPF", "Telefone", "Endere\u00E7o", "Email"
+			}
+		));
+		
+		JButton btnAlterar = new JButton("Alterar");
+//		btnAlterar.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				Long id = (Long) table.getValueAt(table.getSelectedRow(), 0);
+//				String descricao = (String) table.getValueAt(table.getSelectedRow(), 1);
+//				String codBarras = (String) table.getValueAt(table.getSelectedRow(), 2);
+//				Float custo = (Float) table.getValueAt(table.getSelectedRow(), 3);
+//				Float venda = (Float) table.getValueAt(table.getSelectedRow(), 4);
+//				TelaAlterarProduto ta = new TelaAlterarProduto();
+//				ta.setTxtId(id.toString());
+//				ta.setTxtDescricao(descricao);
+//				ta.setTxtCodbarras(codBarras);
+//				ta.setTxtCusto(custo.toString());
+//				ta.setTxtVenda(venda.toString());
+//				ta.getfra().setVisible(true);
+//				atualizaBusca();
+//			}
+//		});
+		btnAlterar.setBounds(236, 290, 89, 23);
+		panel.add(btnAlterar);
+		
+		JButton btnNewButton = new JButton("Excluir");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String cpf = (String) table.getValueAt(table.getSelectedRow(), 2);
+				ClienteDAO dao = new ClienteDAO();
+				dao.delete(cpf, frame);
+				atualizaBusca();
+			}
+		});
+		btnNewButton.setBounds(335, 290, 89, 23);
+		panel.add(btnNewButton);
+	}
+}
